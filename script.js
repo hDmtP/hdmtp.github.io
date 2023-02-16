42b26a01bce4501617e16951a2b6563024c05282
hDmtP <pritamdhara914@gmail.com>
2023-02-16 17:38:48 +0530

    SEM-5 Results and Afterwards...

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-const title_list = document.querySelectorAll("h4")
-const body_list = document.querySelectorAll("p")
 const article_list = document.querySelectorAll("article")
 const searchInput = document.querySelector("[data-search]")
 
-const title_arr = [...title_list]
-const body_arr = [...body_list]
 const article_arr = [...article_list]
-
-// article_arr.forEach(function(element) {
-//     console.log(element['innerText'])
-// })
 
 searchInput.addEventListener("input", (e) => {
     const value = e.target.value.toLowerCase()
